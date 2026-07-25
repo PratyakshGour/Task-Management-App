109fc5cfb52c8c1dc05a99652816b78458bc36ec
PratyakshGour <pratyakshgour1825@gmail.com>
2026-07-25 13:33:17 +0530

    get request done

--- src/tasks/controller.py
+++ src/tasks/controller.py
@@ -1,6 +1,7 @@
 from src.tasks.dtos import TaskSchema
 from sqlalchemy.orm import Session
 from src.tasks.models import TaskModel
+from fastapi import HTTPException
 
 def create_task(body: TaskSchema, db:Session):
     data = body.model_dump()
@@ -16,3 +17,15 @@ def create_task(body: TaskSchema, db:Session):
 
 
     return {"status": "Task Created Succesfully...", "data":new_task}
+
+def get_task(db:Session):
+    tasks = db.query(TaskModel).all()
+    return {"status": "All tasks", "data":tasks}
+
+
+def get_one_task(task_id:int, db:Session):
+    one_task = db.query(TaskModel).get(task_id)
+    if not one_task:
+        return HTTPException(404, detail="Task Id  is Incorrect")
+    
+    return {"status":"Task Fetched Succesfully", "data":one_task}
